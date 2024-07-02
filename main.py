94db184ec4de0bd2496d35a24ff45cac11a11329
SJ Lee <124223633+sjsylee@users.noreply.github.com>
2024-07-02 10:11:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def root():
 async def get_total_order():
     start = time.time()
     # cp = await CP.get_all()
-    ss = await SS.get_all()
+    # ss = await SS.get_all()
     ip = await IP.get_all()
     ew = await EW.get_all()
 
