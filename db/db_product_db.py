e6cc6b29bac59c02ad047be1634521455df34f81
sjsylee <sjsylee123@gmail.com>
2024-12-26 00:05:28 +0900

    no sku case

--- db/db_product_db.py
+++ db/db_product_db.py
@@ -5,8 +5,11 @@ from db.models import DbProductDb
 def get_product_data_by_sku(db: Session, sku: str):
     product_data = db.query(DbProductDb).filter(DbProductDb.sku == sku).first()
     # 에러 핸들 요망
-    pd = product_data.__dict__
-    del pd['_sa_instance_state']
+    try:
+        pd = product_data.__dict__
+        del pd['_sa_instance_state']
+    except AttributeError:
+        pd = {}
 
     return pd
 
